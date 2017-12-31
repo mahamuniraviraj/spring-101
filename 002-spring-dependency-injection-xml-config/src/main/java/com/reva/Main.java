e938b666a39f91b6079fb0a858f6f8ec97828b47
Raviraj Mahamuni <mahamuniraviraj@gmail.com>
2018-01-01 03:18:24 +0530

    closes #3 dependency injection using constructor,property and method

---
+++ 002-spring-dependency-injection-xml-config/src/main/java/com/reva/Main.java
@@ -0,0 +1,77 @@
+/*-
+ *  File    :   Main.java
+ *  Version :   1.0
+ *  Date    :   Dec 30, 2017
+ *  Author  :   Raviraj S Mahamuni
+ *
+    This file is part of 000-hello-world.
+
+    000-hello-world is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    000-hello-world is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 000-hello-world.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+package com.reva;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.reva.service.InventoryService;
+import com.reva.service.ItemService;
+import com.reva.service.OrderService;
+
+/**
+ * @author Raviraj S Mahamuni
+ * @date Dec 30, 2017
+ * @since 1.0
+ */
+public class Main {
+
+	private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		logger.debug("Hello World");
+
+		// create and configure beans
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("my-spring-config.xml");
+
+		// retrieve configured instance
+		ItemService service = context.getBean("itemService", ItemService.class);
+
+		// use configured instance
+		service.printAllItems();
+
+		OrderService orderService = context.getBean("orderService", OrderService.class);
+
+		int price = orderService.getOrderPrice();
+		System.out.println(" Order price " + price);
+
+		InventoryService invService = context.getBean("inventoryService", InventoryService.class);
+
+		Integer stock = invService.getAllStock(1);
+
+		System.out.println("stock " + stock);
+
+		stock = invService.getAllStock(1);
+
+		System.out.println("stock  " + stock);
+		context.close();
+
+	}
+
+}
---
+++ 002-spring-dependency-injection-xml-config/src/main/java/com/reva/dao/ItemDaoImpl.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ * File : ItemDaoImpl.java Version : 1.0 Date : Jan 1, 2018 Author : Raviraj S Mahamuni
+ *
+ * This file is part of 001-spring-context-xml-config. 001-spring-context-xml-config is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version. 000-hello-world is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with 001-spring-context-xml-config. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+package com.reva.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.reva.entity.Item;
+
+/**
+ * @author Raviraj S Mahamuni
+ *
+ *         Jan 1, 2018
+ */
+public class ItemDaoImpl implements ItemDao {
+
+	private static final Logger logger = LoggerFactory.getLogger(ItemDaoImpl.class);
+
+	private static List<Item> items = new ArrayList<>();
+
+	static {
+		Item item1 = new Item(1, "Pen");
+		items.add(item1);
+
+		Item item2 = new Item(2, "Book");
+		items.add(item2);
+
+		Item item3 = new Item(3, "Bag");
+		items.add(item3);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.reva.dao.ItemDao#getItems()
+	 */
+	@Override
+	public List<Item> getItems() {
+
+		logger.debug(" Getting items from dao");
+
+		return items;
+	}
+
+}
